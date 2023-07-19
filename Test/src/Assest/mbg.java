82ed25cefecbe42299171d1d7b8af677c6422e4b
Anushka Isuru <129645615+AnushkaIsuru8@users.noreply.github.com>
2023-07-19 15:36:36 +0530

    Update mbg.java

--- Test/src/Assest/mbg.java
+++ Test/src/Assest/mbg.java
@@ -1,1 +1,13 @@
-\
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/GUIForms/JPanel.java to edit this template
+ */
+package Assest;
+
+/**
+ *
+ * @author Anushka Isuru
+ */
+public class mbg extends javax.swing.JPanel {
+
+}
