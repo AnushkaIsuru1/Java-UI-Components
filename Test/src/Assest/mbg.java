1bae254b342abeccbde5c3ccbc2754b12dc77777
Anushka Isuru <129645615+AnushkaIsuru8@users.noreply.github.com>
2023-07-19 15:36:44 +0530

    Update mbg.java

--- Test/src/Assest/mbg.java
+++ Test/src/Assest/mbg.java
@@ -24,6 +24,22 @@ public class mbg extends javax.swing.JPanel {
      */
     @SuppressWarnings("unchecked")
     // <editor-fold defaultstate="collapsed" desc="Generated Code">//GEN-BEGIN:initComponents
+    private void initComponents() {
+
+        setBackground(new java.awt.Color(6, 14, 28));
+        setBorder(javax.swing.BorderFactory.createLineBorder(new java.awt.Color(0, 145, 196)));
+
+        javax.swing.GroupLayout layout = new javax.swing.GroupLayout(this);
+        this.setLayout(layout);
+        layout.setHorizontalGroup(
+            layout.createParallelGroup(javax.swing.GroupLayout.Alignment.LEADING)
+            .addGap(0, 1120, Short.MAX_VALUE)
+        );
+        layout.setVerticalGroup(
+            layout.createParallelGroup(javax.swing.GroupLayout.Alignment.LEADING)
+            .addGap(0, 577, Short.MAX_VALUE)
+        );
+    }// </editor-fold>//GEN-END:initComponents
 
 
     // Variables declaration - do not modify//GEN-BEGIN:variables
