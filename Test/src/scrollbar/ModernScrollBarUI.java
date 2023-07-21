144bb05334ae10e549660b96807ed4151a7fc32f
Anushka Isuru <129645615+AnushkaIsuru8@users.noreply.github.com>
2023-07-21 15:38:35 +0530

    Update ModernScrollBarUI.java

---
+++ Test/src/scrollbar/ModernScrollBarUI.java
@@ -0,0 +1,14 @@
+package scrollbar;
+
+import java.awt.Color;
+import java.awt.Dimension;
+import java.awt.Graphics;
+import java.awt.Graphics2D;
+import java.awt.Rectangle;
+import java.awt.RenderingHints;
+import javax.swing.BorderFactory;
+import javax.swing.JButton;
+import javax.swing.JComponent;
+import javax.swing.JScrollBar;
+import javax.swing.plaf.basic.BasicScrollBarUI;
+
