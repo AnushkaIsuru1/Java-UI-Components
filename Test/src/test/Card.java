8e5a9a4aabfc4c9713a75028c93d3cbbbe3c9717
Anushka Isuru <129645615+AnushkaIsuru8@users.noreply.github.com>
2023-07-28 15:50:25 +0530

    Update Card.java

--- Test/src/test/Card.java
+++ Test/src/test/Card.java
@@ -20,7 +20,15 @@ public void setData(ModelCard data){
 
         tname.setText("jLabel1");
 
-
+        javax.swing.GroupLayout layout = new javax.swing.GroupLayout(this);
+        this.setLayout(layout);
+        layout.setHorizontalGroup(
+            layout.createParallelGroup(javax.swing.GroupLayout.Alignment.LEADING)
+            .addGroup(layout.createSequentialGroup()
+                .addGap(60, 60, 60)
+                .addComponent(tname, javax.swing.GroupLayout.PREFERRED_SIZE, 164, javax.swing.GroupLayout.PREFERRED_SIZE)
+                .addContainerGap(160, Short.MAX_VALUE))
+        );
 
     }/
 }
