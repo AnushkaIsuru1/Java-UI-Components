43bf2b14685f118adfaab665a879fb8d06958227
Anushka Isuru <129645615+AnushkaIsuru8@users.noreply.github.com>
2023-07-29 17:37:03 +0530

    Update cl.java

--- Test/src/test/cl.java
+++ Test/src/test/cl.java
@@ -5,7 +5,12 @@ import java.awt.Font;
 import java.io.InputStream;
 
 public class cl {
+        public static Font openFontTTF( float  fontSize) {
+            String fontPath =   "1.ttf";
+            
 
+          }
+        
 
 
 }
