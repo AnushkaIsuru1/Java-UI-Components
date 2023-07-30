33e39c44f15f3dafbd5a4c62fe08e227c4cb3f22
Anushka Isuru <129645615+AnushkaIsuru8@users.noreply.github.com>
2023-07-30 17:38:09 +0530

    Update combo2.java

---
+++ Test/src/test/combo2.java
@@ -0,0 +1,13 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package test;
+import Assest.skin;
+import java.awt.Color;
+import java.awt.FlowLayout;
+import java.awt.Label;
+import javax.swing.JButton;
+import javax.swing.JLabel;
+import javax.swing.UIManager;
+import javax.swing.plaf.ColorUIResource;
