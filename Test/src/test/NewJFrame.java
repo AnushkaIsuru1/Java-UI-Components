61be338792725483cd491082fb8927d77a09e4ff
Anushka Isuru <129645615+AnushkaIsuru8@users.noreply.github.com>
2023-08-06 18:05:38 +0530

    Update NewJFrame.java

---
+++ Test/src/test/NewJFrame.java
@@ -0,0 +1,11 @@
+
+package test;
+
+import Assest.skin;
+import java.awt.Color;
+import java.util.Date;
+import javax.swing.SwingUtilities;
+import javax.swing.UIManager;
+import javax.swing.plaf.ColorUIResource;
+
+
