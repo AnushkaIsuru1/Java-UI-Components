7ae02870853079538e18cd008466259960f8b233
Anushka Isuru <129645615+AnushkaIsuru8@users.noreply.github.com>
2023-08-06 18:05:45 +0530

    Update NewJFrame.java

--- Test/src/test/NewJFrame.java
+++ Test/src/test/NewJFrame.java
@@ -12,5 +12,20 @@ import javax.swing.plaf.ColorUIResource;
 
 public class NewJFrame extends javax.swing.JFrame {
 
+   
 
+
+
+
+
+
+    Color b1= new Color(0, 127, 240);
+    // Variables declaration - do not modify//GEN-BEGIN:variables
+    private test.Combobox combobox1;
+    private test.Combobox combobox2;
+    private javax.swing.JButton jButton1;
+    private javax.swing.JComboBox<String> jComboBox1;
+    private test.nW nW1;
+    private test.nW nW2;
+    // End of variables declaration//GEN-END:variables
 }
