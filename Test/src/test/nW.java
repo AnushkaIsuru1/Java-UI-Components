5b22348d27acfd3b1ffb61a42a92587dee5fb671
Anushka Isuru <129645615+AnushkaIsuru8@users.noreply.github.com>
2023-08-12 19:42:18 +0530

    Update nW.java

--- Test/src/test/nW.java
+++ Test/src/test/nW.java
@@ -2,3 +2,13 @@
  * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
  * Click nbfs://nbhost/SystemFileSystem/Templates/GUIForms/JPanel.java to edit this template
  */
+package test;
+
+import Assest.skin;
+import java.awt.image.ColorModel;
+import  javax.swing.JLabel;
+import javax.swing.JPanel;
+public class nW extends JPanel{
+
+
+}
