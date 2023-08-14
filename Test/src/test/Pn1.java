0126f64b2ece03238d0c6741983d0c45f7b26d25
Anushka Isuru <129645615+AnushkaIsuru8@users.noreply.github.com>
2023-08-14 20:34:48 +0530

    Update Pn1.java

---
+++ Test/src/test/Pn1.java
@@ -0,0 +1,10 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/GUIForms/JPanel.java to edit this template
+ */
+package test;
+
+/**
+ *
+ * @author Anushka Isuru
+ */
