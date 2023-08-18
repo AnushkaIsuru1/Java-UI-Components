f23303ad064386721ff88ce351c0ada39b286fef
Anushka Isuru <129645615+AnushkaIsuru8@users.noreply.github.com>
2023-08-18 20:36:50 +0530

    Update ScrollBarCustome.java

---
+++ Test/src/test/ScrollBarCustome.java
@@ -0,0 +1,14 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package test;
+
+import java.awt.Color;
+import java.awt.Dimension;
+import javax.swing.JScrollBar;
+
+/**
+ *
+ * @author Anushka Isuru
+ */
