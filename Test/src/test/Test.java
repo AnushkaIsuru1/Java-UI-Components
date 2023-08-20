e1c5c9d40268a925f4b5a656f3ce2966437aa9bc
Anushka Isuru <129645615+AnushkaIsuru8@users.noreply.github.com>
2023-08-20 20:41:06 +0530

    Update Test.java

---
+++ Test/src/test/Test.java
@@ -0,0 +1,17 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package scrollbar;
+
+import java.awt.Color;
+import javax.swing.JScrollBar;
+
+/**
+ *
+ * @author RAVEN
+ */
+public class Test extends javax.swing.JFrame {
+
+}
