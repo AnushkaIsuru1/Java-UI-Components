9131e2b549cea409cb9861a95fa9052e1c648a6e
Anushka Isuru <129645615+AnushkaIsuru8@users.noreply.github.com>
2023-08-22 20:46:06 +0530

    Update ModernScrollBarUI.java

---
+++ Scrollbar/src/scrollbar/ModernScrollBarUI.java
@@ -0,0 +1,19 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package scrollbar;
+
+import java.awt.Color;
+import java.awt.Dimension;
+import java.awt.Graphics;
+import java.awt.Graphics2D;
+import java.awt.Rectangle;
+import java.awt.RenderingHints;
+import javax.swing.BorderFactory;
+import javax.swing.JButton;
+import javax.swing.JComponent;
+import javax.swing.JScrollBar;
+import javax.swing.plaf.basic.BasicScrollBarUI;
+
+
