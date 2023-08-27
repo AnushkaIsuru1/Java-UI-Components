6671360166c56861a8fe82e4ca6482b553c81a76
Anushka Isuru <129645615+AnushkaIsuru8@users.noreply.github.com>
2023-08-27 20:53:56 +0530

    Create ScrollBarCustome.java

---
+++ Scrollbar/src/scrollbar/ScrollBarCustome.java
@@ -0,0 +1,14 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Main.java to edit this template
+ */
+package scrollbar;
+
+import java.awt.Color;
+import java.awt.Dimension;
+import javax.swing.JScrollBar;
+
+/**
+ *
+ * @author Anushka Isuru
+ */
