d856a82e159f318334bc0dd621338c57a3199dfe
Anushka Isuru <129645615+AnushkaIsuru8@users.noreply.github.com>
2023-08-28 20:59:05 +0530

    Update Test.java

---
+++ Scrollbar/src/scrollbar/Test.java
@@ -0,0 +1,18 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/GUIForms/JFrame.java to edit this template
+ */
+package scrollbar;
+
+import java.awt.Color;
+import javax.swing.JScrollBar;
+
+/**
+ *
+ * @author Anushka Isuru
+ */
+public class Test extends javax.swing.JFrame {
+
+
+
+}
