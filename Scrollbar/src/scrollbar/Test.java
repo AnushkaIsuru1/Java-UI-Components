a413f0570a1d64eda0cc98e433255689c4dc7607
Anushka Isuru <129645615+AnushkaIsuru8@users.noreply.github.com>
2023-08-29 20:59:40 +0530

    Update Test.java

--- Scrollbar/src/scrollbar/Test.java
+++ Scrollbar/src/scrollbar/Test.java
@@ -73,7 +73,14 @@ public class Test extends javax.swing.JFrame {
         } catch (javax.swing.UnsupportedLookAndFeelException ex) {
             java.util.logging.Logger.getLogger(Test.class.getName()).log(java.util.logging.Level.SEVERE, null, ex);
         }
+        //</editor-fold>
 
+        /* Create and display the form */
+        java.awt.EventQueue.invokeLater(new Runnable() {
+            public void run() {
+                new Test().setVisible(true);
+            }
+        });
     }
 
     // Variables declaration - do not modify//GEN-BEGIN:variables
