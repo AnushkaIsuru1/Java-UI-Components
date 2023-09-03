ab11edc1813f5683d6179af440a6fcc0853acf3f
Anushka Isuru <129645615+AnushkaIsuru8@users.noreply.github.com>
2023-09-03 21:20:58 +0530

    Update LMS.java

--- LMS/src/LMS.java
+++ LMS/src/LMS.java
@@ -3,6 +3,12 @@ package lms;
 
 public class LMS {
 
-
+    public static void main(String[] args) {
+                java.awt.EventQueue.invokeLater(new Runnable() {
+            public void run() {
+                new Notice().setVisible(true);
+            }
+        });
+    }
     
 }
