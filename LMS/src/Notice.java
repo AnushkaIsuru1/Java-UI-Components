a7ce20603ef9f7fb0f67aee44165b1e5944da9a6
Anushka Isuru <129645615+AnushkaIsuru8@users.noreply.github.com>
2023-09-12 21:31:53 +0530

    Update Notice.java

--- LMS/src/Notice.java
+++ LMS/src/Notice.java
@@ -210,6 +210,18 @@ public class Notice extends javax.swing.JFrame {
             }
         });
 
+        jTable1.setModel(new javax.swing.table.DefaultTableModel(
+            new Object [][] {
+                {null, null, null, null},
+                {null, null, null, null},
+                {null, null, null, null},
+                {null, null, null, null}
+            },
+            new String [] {
+                "Title 1", "Title 2", "Title 3", "Title 4"
+            }
+        ));
+
 
 
     // Variables declaration - do not modify//GEN-BEGIN:variables
