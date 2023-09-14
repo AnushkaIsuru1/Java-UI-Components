a57e29e376f2902cb6c9a5fe11e0ba4da07a5733
Anushka Isuru <129645615+AnushkaIsuru8@users.noreply.github.com>
2023-09-14 21:32:52 +0530

    Update Notice.java

--- LMS/src/Notice.java
+++ LMS/src/Notice.java
@@ -307,6 +307,18 @@ public class Notice extends javax.swing.JFrame {
         // TODO add your handling code here:
     }//GEN-LAST:event_textField5ActionPerformed
 
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String args[]) {
+        /* Set the Nimbus look and feel */
+        //<editor-fold defaultstate="collapsed" desc=" Look and feel setting code (optional) ">
+        /* If Nimbus (introduced in Java SE 6) is not available, stay with the default look and feel.
+         * For details see http://download.oracle.com/javase/tutorial/uiswing/lookandfeel/plaf.html 
+         */
+
+
+    }
 
     // Variables declaration - do not modify//GEN-BEGIN:variables
     private javax.swing.JButton jButton1;
