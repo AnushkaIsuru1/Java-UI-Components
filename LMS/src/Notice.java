f8a47f5ea19d190e3cd1c313b6e5a983166a9f21
Anushka Isuru <129645615+AnushkaIsuru8@users.noreply.github.com>
2023-09-14 21:32:57 +0530

    Update Notice.java

--- LMS/src/Notice.java
+++ LMS/src/Notice.java
@@ -332,7 +332,17 @@ public class Notice extends javax.swing.JFrame {
         } catch (javax.swing.UnsupportedLookAndFeelException ex) {
             java.util.logging.Logger.getLogger(Notice.class.getName()).log(java.util.logging.Level.SEVERE, null, ex);
         }
+        //</editor-fold>
+        //</editor-fold>
+        //</editor-fold>
+        //</editor-fold>
 
+        /* Create and display the form */
+        java.awt.EventQueue.invokeLater(new Runnable() {
+            public void run() {
+                new Notice().setVisible(true);
+            }
+        });
     }
 
     // Variables declaration - do not modify//GEN-BEGIN:variables
