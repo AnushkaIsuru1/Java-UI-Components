590f4f65016d04d0ecb4dcf04173fda93d8b6213
Anushka Isuru <129645615+AnushkaIsuru8@users.noreply.github.com>
2023-09-23 22:06:45 +0530

    Update usersFrame.java

---
+++ LMS/src/usersFrame.java
@@ -0,0 +1,16 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/GUIForms/JFrame.java to edit this template
+ */
+package lms;
+
+/**
+ *
+ * @author Anushka Isuru
+ */
+public class usersFrame extends javax.swing.JFrame {
+
+
+
+
+}
