927b016ed0e0e10e229bb5a5a97c7a6f8adf380a
Anushka Isuru <129645615+AnushkaIsuru8@users.noreply.github.com>
2023-09-27 22:09:57 +0530

    Update usersFrame.java

--- LMS/src/usersFrame.java
+++ LMS/src/usersFrame.java
@@ -236,6 +236,10 @@ public class usersFrame extends javax.swing.JFrame {
         jComboBox1.setForeground(new java.awt.Color(255, 255, 255));
         jComboBox1.setModel(new javax.swing.DefaultComboBoxModel<>(new String[] { "BST", "ET", "ICT" }));
 
+        jComboBox2.setBackground(new java.awt.Color(65, 65, 65));
+        jComboBox2.setForeground(new java.awt.Color(255, 255, 255));
+        jComboBox2.setModel(new javax.swing.DefaultComboBoxModel<>(new String[] { "Male", "Female" }));
+
 
     }// </editor-fold>//GEN-END:initComponents
 
