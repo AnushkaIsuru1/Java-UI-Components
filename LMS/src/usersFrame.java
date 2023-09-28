f0a7d5f58b6260574f9fa30bbecf4bd3cc6f4f22
Anushka Isuru <129645615+AnushkaIsuru8@users.noreply.github.com>
2023-09-28 22:09:56 +0530

    Update usersFrame.java

--- LMS/src/usersFrame.java
+++ LMS/src/usersFrame.java
@@ -302,6 +302,17 @@ public class usersFrame extends javax.swing.JFrame {
         textField5.setForeground(new java.awt.Color(255, 255, 255));
         textField5.setText("textField1");
 
+        jTable1.setModel(new javax.swing.table.DefaultTableModel(
+            new Object [][] {
+                {null, null, null, null},
+                {null, null, null, null},
+                {null, null, null, null},
+                {null, null, null, null}
+            },
+            new String [] {
+                "Title 1", "Title 2", "Title 3", "Title 4"
+            }
+        ));
 
 
 
