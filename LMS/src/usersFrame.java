f0afe1727bd43e98761ef88ff2c4d1727d13346d
Anushka Isuru <129645615+AnushkaIsuru8@users.noreply.github.com>
2023-09-29 22:11:09 +0530

    Update usersFrame.java

--- LMS/src/usersFrame.java
+++ LMS/src/usersFrame.java
@@ -445,6 +445,10 @@ public class usersFrame extends javax.swing.JFrame {
         pack();
     }// </editor-fold>//GEN-END:initComponents
 
+    private void jButton1ActionPerformed(java.awt.event.ActionEvent evt) {//GEN-FIRST:event_jButton1ActionPerformed
+        // TODO add your handling code here:
+    }//GEN-LAST:event_jButton1ActionPerformed
+
 
     // Variables declaration - do not modify//GEN-BEGIN:variables
     private javax.swing.JButton jButton1;
